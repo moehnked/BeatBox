97c3c45cd6822058cceaec9af2359d6ce519c654
moehnked <40707466+moehnked@users.noreply.github.com>
2019-11-24 15:54:15 -0800

    Initial commit

---
+++ app/src/main/java/com/dmoneyextreme/beatbox/SoundViewModel.java
@@ -0,0 +1,31 @@
+package com.dmoneyextreme.beatbox;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+public class SoundViewModel extends BaseObservable {
+    private Sound mSound;
+    private BeatBox mBeatbox;
+
+    public SoundViewModel(BeatBox beatBox){
+        mBeatbox = beatBox;
+    }
+
+    public Sound getSound(){
+        return mSound;
+    }
+
+    public void setSound(Sound sound){
+        mSound = sound;
+        notifyChange();
+    }
+
+    @Bindable
+    public String getTitle(){
+        return mSound.getName();
+    }
+
+    public BeatBox getBeatBox(){
+        return mBeatbox;
+    }
+}
